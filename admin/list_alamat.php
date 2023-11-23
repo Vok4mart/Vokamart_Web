b9737bf0ae830b4b82d6a9a086cc81145e64873b
Aulia <auliaummatulh@gmail.com>
2023-11-23 07:46:05 +0700

    update perubahan
    perubahan klik button dan responsive

--- admin/list_alamat.php
+++ admin/list_alamat.php
@@ -17,43 +17,33 @@
   <!-- list alamat -->
 <div class="container">
 <!-- seach bar -->
-<div class="row-sm-1  ">
-  <div class="card no-border">
-  <div class="card-body d-flex">
-  <form class=" form-search d-flex" role="search">
-    <button type="submit">Cari</button>
-        <input class="form-control" type="search" >
-      </form>
 
-    <a href="tambahalamat.php" class="btn btn-primary ms-auto align-self-center" style="padding: 5px 50px">Tambahkan Alamat</a>
+   
+  
+  <div class="row-sm-1 mb-4 ">
+  <div class="card">
+  <div class="card-body card-search-button d-flex">
+    <form class=" form-search d-flex" role="search">
+      <button type="submit">Cari</button>
+          <input class="form-control" type="search" >
+    </form>
+
+      <a href="tambahalamat.php" class="btn btn-tambah-alamat btn-danger ms-auto align-self-center" style="padding: 5px 50px">Tambahkan Alamat</a>
   </div>
 </div>
   </div>
-    <!-- search bar -->
+   <!-- search bar -->
+
     <!-- card 1 -->
   <div class="row-sm-1 mb-4 ">
   <div class="card">
-  <div class="card-body d-flex">
+  <div class="card-body d-flex card-border">
     <div class="wadah">
       <h5 class="card-title">Nama Penerima</h5>
       <h5 class="card-title">No.HP</h5>
       <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quas nostrum iure?</p>
     </div>
-    <a href="#" class="btn btn-primary ms-auto align-self-center px-5">pilih</a>
-  </div>
-</div>
-  </div>
-
-  <!-- card 2 -->
-  <div class="row-sm-1 mb-4 ">
-  <div class="card">
-  <div class="card-body d-flex">
-    <div class="wadah">
-      <h5 class="card-title">Nama Penerima</h5>
-      <h5 class="card-title">No.HP</h5>
-      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quas nostrum iure?</p>
-    </div>
-    <a href="#" class="btn btn-primary ms-auto align-self-center px-5">pilih</a>
+    <button class="btn btn-danger ms-auto align-self-center px-5" onclick="changeCardColor(document.getElementById('card1'))">pilih</button>
   </div>
 </div>
   </div>
@@ -66,5 +56,25 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+// Variabel untuk menyimpan referensi card saat ini yang memiliki warna merah
+var currentRedCard = null;
+
+function changeCardColor(cardBorder) {
+    // Memeriksa apakah card saat ini memiliki warna merah
+    if (currentRedCard !== null) {
+        // Menghapus warna merah dari card saat ini
+        currentRedCard.style.borderColor = '';
+    }
+
+    // Set card saat ini sebagai card yang memiliki warna merah
+    currentRedCard = cardBorder;
+
+    // Set warna merah pada card yang baru diklik
+    cardBorder.style.borderColor = 'red';
+}
+</script>
+
+
 </body>
 </html>
