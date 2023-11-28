ae527b234ff306cbdde99268537bc11de485767c
Aulia <auliaummatulh@gmail.com>
2023-11-28 08:25:52 +0700

    fitur about

---
+++ admin/contact.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- style boostrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">  
+   <!-- fonts -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&family=Poppins:ital,wght@0,100;0,300;0,500;0,900;1,100&display=swap" rel="stylesheet">
+    <!-- my style  -->
+    <link rel="stylesheet" href="../css/contact.css">
+    <title>Document</title>
+</head>
+<body>
+        <!-- navbar -->
+<nav class="navbar navbar-expand-lg  mx-4">
+<div class="container-fluid">
+    <a class="navbar-brand" href="home.php">
+      <img src="" alt="vokamart">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto"> <!-- Menggunakan ms-auto untuk meratakan ke kanan -->
+        <li class="nav-item mx-4">
+          <a class="nav-link" href="produk.php">Produk Kami</a>
+        </li>
+        <li class="nav-item mx-4">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item mx-4">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item mx-4">
+          <a class="nav-link" href="akun.php">Akun</a>
+        </li>
+        <li class="nav-item mx-4">
+          <a class="nav-link" href="keranjang.php">Keranjang</a>
+        </li>
+      </ul>
+    </div>
+</div>
+</nav>
+<!-- akhir navbar -->
+
+
+
+
+  <!-- Kontak Kami -->
+    <div class="row">
+    <div class="col-md-6 contact-info">
+    <h2 class="text-center">CONTACT US</h2>
+    <p class="text-center">Kami akan selalu ada dan akan melayani anda dengan layanan prima</p>
+    <p class="address">Jl. Kartini no. 1 Jember</p>
+    <p class="email" >info@vok4mart.smepa.id</p>
+    <p class="handphone" >0895-2256-2432</p>
+  </div>
+        <div class="col-md-6">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.2472972394385!2d113.65460917421316!3d-8.177823881973607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd693fadeb3b501%3A0x2433008be7cde2d1!2sJl.%20Bandeng%20No.22%2C%20Mrapa%2C%20Dukuh%20Mencek%2C%20Kec.%20Sukorambi%2C%20Kabupaten%20Jember%2C%20Jawa%20Timur%2068131!5e0!3m2!1sid!2sid!4v1697676450665!5m2!1sid!2sid"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="map"
+        >
+        </iframe>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<!-- Js -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</body>
+</html>
