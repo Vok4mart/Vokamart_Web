caa9c1f5009cc43cb59c631b9bf6834bf6946009
Aulia <auliaummatulh@gmail.com>
2023-11-28 08:25:44 +0700

    fitur contact

--- admin/about.php
+++ admin/about.php
@@ -46,8 +46,95 @@
 </nav>
 <!-- akhir navbar -->
 
-<div class="gambar">
-<img src="../images/Sewa Kantor Strategis - Bisnis Digital Dalam Negeri Dongkrak Sewa Kantor di Pusat Jakarta 1.png" class="img-fluid" alt="...">
+
+
+<div class="container">
+  <!-- gambar about ud -->
+  <div class="row">
+    <div class="col">
+    <div class="gambar">
+      <img src="../images/Sewa Kantor Strategis - Bisnis Digital Dalam Negeri Dongkrak Sewa Kantor di Pusat Jakarta 1.png" class="img-fluid" alt="...">
+    </div>
+    </div>
+  </div>
+  <!-- tulisan welcome -->
+  <div class="row welcome">
+    <div class="col">   
+    <div class="about-welcome text-center">
+      <h2>WELCOME TO <span class="about-welcome-v" >V</span >OK4<span class="about-welcome-merah" ><span  class="about-welcome-m">M</span>ART</span> STORE </h2>
+      <p>SMKN 4 JEMBER telah mendirikan TOKO ONLINE bernama VOK4MART. Kami menyediakan beragam kebutuhan Anda dengan Pelayanan yang cepat, aman dan terpercaya.</p>
+    </div>
+    </div>
+  </div>
+  <!-- gambar dan tulisan beli sekaang -->
+  <div class="konten d-flex  ">
+    <div class="row about-text-gambar flex-column-reverse flex-md-row">
+      <div class="col-lg-7 col-sm-12 order-lg-first">
+        <div class="about-text">
+          <p>Silahkan anda berbelanja di Toko Online Kami VOK4MART, Kami akan melayani semua kebutuhan harian anda dengan Kecepatan dan Kesempurnaan Layanan Kami.
+              Layanan dari Kami sudah dipersiapkan dengan baik dengan harapan semua kebutuhan harian anda terpenuhi dengan cepat dan terjangkau.</p>
+          <p>Temukan Produk Produk terbaik kami disini !</p>
+          <a href="produk.php" class="btn btn-light text-danger" >BELI SEKARANG</a>
+        </div>
+      </div>
+      <div class="col-lg-5 col-sm-12 order-lg-last">
+        <div class="img-area ">
+          <img src="../images/Free Photo _ Group of people working out business plan in an office 1.png" class="img-fluid"  alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- card choose us -->
+  <h3 class="text-center mt-3" >WHY CHOOSE US</h3>
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+   
+  <div class="col">
+    <div class="card h-100" ">
+      <img src="../images/🦆 illustration _Online shopping_.png" class="card-img-top w-50 mx-auto mt-3 img-fluid" alt="...">
+      <div class="card-body text-center" style="margin-top: 28px;" >
+        <h5 class="card-title">ONLINE SHOP</h5>
+        <p class="card-text ">Vok4mart memberikan pelayanan prima dalam setiap transaksi.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../images/🦆 icon _special offer_.png" class="card-img-top w-50 mx-auto mt-3 img-fluid" alt="...">
+      <div class="card-body text-center">
+        <h5 class="card-title">PRODUK BERKUALITAS</h5>
+        <p class="card-text ">Vok4mart menyediakan Produk yang berasal dari bahan berkualitas terbaik.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100" >
+      <img src="../images/🦆 icon _waterproof packaging_.png" class="card-img-top w-50 mx-auto mt-3 img-fluid" alt="...">
+      <div class="card-body text-center">
+        <h5 class="card-title">KEMASAN YANG AMAN</h5>
+        <p class="card-text ">VOK4MART menggunakan bahan kemasan aman terjaga dari kerusakan selama pengiriman barang</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../images/🦆 icon _offer_.png" class="card-img-top w-50 mx-auto mt-3 img-fluid" alt="...">
+      <div class="card-body text-center">
+        <h5 class="card-title">PENAWARAN TERBAIK</h5>
+        <p class="card-text ">VOK4MART menawarkan harga murah dan bersaing dengan produk berkualitas terbaik</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
 </div>
 
 
